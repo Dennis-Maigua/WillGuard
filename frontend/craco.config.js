f6856e45b8479fdc119b22e17727d5915c69cbd3
Dennis-Maigua <dennis.maigua@strathmore.edu>
2024-05-22 14:59:58 +0300

    update file

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,8 +1,12 @@
 // craco.config.js
 module.exports = {
   style: {
-    postcss: {
-      plugins: [require("tailwindcss"), require("autoprefixer")],
+    // why use postcssOptions? -> https://github.com/dilanx/craco/issues/353
+    postcssOptions: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
     },
   },
-};
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,7 @@
-function App() {
+export default function App() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       <p className="text-gray-500 text-2xl">React Tailwind</p>
     </div>
   );
 }
-
-export default App;
