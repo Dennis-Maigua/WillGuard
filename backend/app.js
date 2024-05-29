a35818ac467f1e5faa683e9e135a9e9a604f2540
Dennis-Maigua <dennis.maigua@strathmore.edu>
2024-05-29 12:03:07 +0300

    update file

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,12 @@ const cors = require('cors');
 const userRouter = require('./routes/user');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors());
 app.use('/api/user', userRouter);
 
-app.listen(PORT, () => {
-    console.log(`App is running on ${PORT}`);
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
 });
