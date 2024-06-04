9f824a18fb004ff1d7827cb65c68b81bd96d0661
Dennis-Maigua <dennis.maigua@strathmore.edu>
2024-06-04 16:40:59 +0300

    update file

--- application/app/api/client.js
+++ application/app/api/client.js
@@ -4,7 +4,7 @@ const Client = axios.create({
     baseURL: 'https://629a-2c0f-fe38-2406-6b0c-ac41-1d79-3d49-a812.ngrok-free.app'
 
     // for emulator: 'http://192.168.100.142:8000' 
-    // for device: 'https://09c3-41-80-112-22.ngrok-free.app'
+    // for device: 'https://xxxx-xx-xxx.ngrok-free.app'
 
 });
 
