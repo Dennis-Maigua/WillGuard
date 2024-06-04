b15ff56652021da84448fc757654d0dbfb8159d2
Dennis-Maigua <dennis.maigua@strathmore.edu>
2024-06-04 16:31:38 +0300

    update file

--- backend/models/user.js
+++ backend/models/user.js
@@ -26,17 +26,17 @@ const userSchema = new mongoose.mongoose.Schema({
     }
 });
 
-userSchema.pre('save', async function(next){
-    if(this.isModified('password')) {
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
         const hash = await bcrypt.hash(this.password, 8);
         this.password = hash;
     }
     next();
-})
+});
 
-userSchema.methods.comparePassword = async function(password) {
+userSchema.methods.comparePassword = async function (password) {
     const result = await bcrypt.compareSync(password, this.password);
     return result;
-}
+};
 
 module.exports = mongoose.model('User', userSchema);
